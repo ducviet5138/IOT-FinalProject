4cbca44678283d202246183f54bd3c3b5e218310
Bui Do Duy Quan <bddquan@gmail.com>
2023-08-03 11:00:53 +0700

    move code to my_device

--- src/device/my_device.h
+++ src/device/my_device.h
@@ -6,6 +6,8 @@
 #include "device/device_pir.h"
 #include "device/device_relay.h"
 #include "device/device_IR.h"
+#include <PubSubClient.h>
+#include <WiFi.h>
 
 class MyDevice
 {
@@ -15,6 +17,15 @@ class MyDevice
         DevicePIR pir;
         DeviceRelay relay;
         DeviceIR ir;
+
+        bool working_mode;
+        bool person_in_room;
+
+        const char* server = "broker.hivemq.com";
+        const String main_channel = "/GDrpD2J3jxvzQEy7vGOn/";
+        WiFiClient espClient;
+        PubSubClient client;
+        
     public:
         void SetUp()
         {
@@ -23,7 +34,26 @@ class MyDevice
             pir.SetUp();
             relay.SetUp();
             ir.SetUp();
-        }       
+
+            working_mode = 1;
+            person_in_room = 1;
+
+            client = PubSubClient(espClient);
+            client.setServer(server, 1883);
+            client.setCallback(callback);
+        }  
+
+        void callback(char* topic, byte* message, unsigned int length);
+        {
+            String buffer = "";
+            for (int i = 0; i < length; i++) buffer += char(message[i]);    
+            Serial.println(buffer);
+        }    
+
+        const char* GetChannel(String pram)
+        {
+            return (main_channel + pram).c_str();
+        } 
 };
 
 #endif
