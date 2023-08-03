d497ca4498ce13a0e2d613ba975e2d7a7e2e7db5
ducviet5138 <ducviet5138@gmail.com>
2023-08-03 11:31:34 +0700

    Update nhiều cái

---
+++ src/device/my_device.cpp
@@ -0,0 +1,73 @@
+#include "my_device.h"
+
+void MyDevice::SetUp()
+{
+    dht.SetUp();
+    lcd.SetUp();
+    pir.SetUp();
+    relay.SetUp();
+    ir.SetUp();
+
+    working_mode = 1;
+    person_in_room = 1;
+
+    client = PubSubClient(espClient);
+    client.setServer(server, 1883);
+    client.setCallback(callback);
+
+    WiFi.begin(ssid, pw);
+}
+
+void MyDevice::reconnect()
+{
+    while (!client.connected())
+    {
+        String clientId = "ESP32Client-21127112-21127141-21127203";
+
+        Serial.print("Connecting to server... ");
+        if (client.connect(clientId.c_str()))
+        {
+            Serial.println("Connected!");
+            client.subscribe(GetChannel("WorkingMode"));
+        }
+        else
+        {
+            Serial.println("Failed!");
+            delay(5000);
+        }
+    }
+}
+
+void MyDevice::callback(char* topic, byte* message, unsigned int length)
+{
+    String buffer = "";
+    for (int i = 0; i < length; i++) buffer += char(message[i]);    
+    Serial.println(buffer);   
+}
+
+void MyDevice::ReconnectToServer()
+{
+    if (!client.connected()) reconnect();
+    client.loop();
+}
+
+void MyDevice::Sync(String param, String value)
+{
+    client.publish(GetChannel(param), value.c_str());
+}
+
+void MyDevice::SyncTempAndHumid()
+{
+    Sync("temperature", dht.GetTemperature());
+    Sync("humid", dht.GetHumid());
+}
+
+const char* MyDevice::GetChannel(String param)
+{
+    return (main_channel + param).c_str();
+}
+
+void MyDevice::GetPersonStatus()
+{
+    person_in_room = pir.IsMotion();
+}
--- src/device/my_device.h
+++ src/device/my_device.h
@@ -29,78 +29,18 @@ class MyDevice
         const String ssid = "Wokwi-GUEST";
         const String pw = "";
     public:
-        void SetUp()
-        {
-            dht.SetUp();
-            lcd.SetUp();
-            pir.SetUp();
-            relay.SetUp();
-            ir.SetUp();
+        void SetUp();
 
-            working_mode = 1;
-            person_in_room = 1;
-
-            client = PubSubClient(espClient);
-            client.setServer(server, 1883);
-            client.setCallback(callback);
-
-            WiFi.begin(ssid, pw);
-        }  
-
-        void reconnect()
-        {
-            while (!client.connected())
-            {
-                String clientId = "ESP32Client-21127112-21127141-21127203";
-
-                Serial.print("Connecting to server... ");
-                if (client.connect(clientId.c_str()))
-                {
-                    Serial.println("Connected!");
-                    client.subscribe(GetChannel("WorkingMode"));
-                }
-                else
-                {
-                    Serial.println("Failed!");
-                    delay(5000);
-                }
-            }
-        }
-
-
+        const char* GetChannel(String pram);
         void callback(char* topic, byte* message, unsigned int length);
-        {
-            String buffer = "";
-            for (int i = 0; i < length; i++) buffer += char(message[i]);    
-            Serial.println(buffer);
-        }    
+        void reconnect();
+        void ReconnectToServer();
 
-        const char* GetChannel(String pram)
-        {
-            return (main_channel + pram).c_str();
-        } 
+        void Sync();
+        void SyncTempAndHumid();
 
         void GetPersonStatus()
-        {
-            person_in_room = pir.IsMotion();
-        }
-
-        void Sync(String param, String value)
-        {
-            client.publish(GetChannel(pram), value.c_str());
-        }
-
-        void SyncTempAndHumid()
-        {
-            Sync("temperature", dht.GetTemperature());
-            Sync("humid", dht.GetHumidity());
-        }
-
-        void ReconnectToServer()
-        {
-            if (!client.connected()) reconnect();
-            client.loop();
-        }
+        
 };
 
 #endif
