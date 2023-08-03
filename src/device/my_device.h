40c74ea93f5d0db50fb5ff7f623f391a3be447c4
ducviet5138 <ducviet5138@gmail.com>
2023-08-03 10:32:10 +0700

    Rename

--- src/device/my_device.h
+++ src/device/my_device.h
@@ -1,5 +1,15 @@
+#ifndef MY_DEVICE_H
+#define MY_DEVICE_H
+
 #include "device/device_dht.h"
 #include "device/device_dht.h"
 #include "device/device_pir.h"
 #include "device/device_relay.h"
 #include "device/device_IR.h"
+
+class MyDevice
+{
+
+};
+
+#endif
